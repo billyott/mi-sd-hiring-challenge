77debfb6ad03663cb02f3a75f88165168656a44a
Billy Ott <willrichott@gmail.com>
2021-07-20 19:04:18 -0400

    add initial html

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
 import { convertDate } from "./utils";
 
-console.log("hello world");
+//test api calls
+
+// fetch("https://se-weather-api.herokuapp.com/api/v1/geo?zip_code=10128")
+//   .then(rsp => rsp.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.log('error', error));
+
+//   fetch("https://se-weather-api.herokuapp.com/api/v1/forecast?latitude=34.09&longitude=-104.2&date=01/24/2021")
+//   .then(rsp => rsp.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.log('error', error));
