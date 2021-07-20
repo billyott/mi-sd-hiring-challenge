f6ecb5c624b5be9cf7ba3e8a3f29d103c159435f
Billy Ott <willrichott@gmail.com>
2021-07-20 19:31:11 -0400

    clean up fetch functions

--- src/app.js
+++ src/app.js
@@ -1,43 +1,34 @@
 import { convertDate } from "./utils";
 
-//test api calls
-
-// fetch("https://se-weather-api.herokuapp.com/api/v1/geo?zip_code=10128")
-//   .then(rsp => rsp.json())
-//   .then(data => console.log(data))
-//   .catch(error => console.log('error', error));
-
-//   fetch("https://se-weather-api.herokuapp.com/api/v1/forecast?latitude=34.09&longitude=-104.2&date=01/24/2021")
-//   .then(rsp => rsp.json())
-//   .then(data => console.log(data))
-//   .catch(error => console.log('error', error));
-
 //*******target elements*******
 
 const subheader = document.getElementById('subheader');
 const forecastContainer = document.getElementById('forecast-container');
 
 const BASEURL = 'https://se-weather-api.herokuapp.com/api/v1/';
-const geoEndPoint = 'geo?zip_code=';
+const geoEndPoint = 'geo';
 const forecastEndPoint = 'forecast';
 
 
 //*******functions*******
 
-//call geo endpoint with default value for zip param
-function getGeoData(zip = '10128') {
+//call weather endpoint with default value for zip param
+function getForecastData(geoData) {
 
-    return fetch(BASEURL+geoEndPoint+zip)
+    const date = formatDate(getCurrentDate());
+
+    fetch(`${BASEURL}${forecastEndPoint}?latitude=${geoData.latitude}&longitude=${geoData.longitutde}&date=${date}`)
         .then(rsp => rsp.json())
-        .then(data => data)
+        .then(data => console.log(data)) //PLACEHOLDER
         .catch(error => console.log('error', error));
 }
 
-//call weather endpoint with default value for zip param
-function getForecastData(geoData, date = '') {
-    fetch(BASEURL+geoEndPoint+zip)
+//call geo endpoint with default value for zip param
+function getGeoData(zip = '10128') {
+
+    return fetch(BASEURL+geoEndPoint+'?zip_code='+zip)
         .then(rsp => rsp.json())
-        .then(data => console.log(data))
+        .then(data => getForecastData(data))
         .catch(error => console.log('error', error));
 }
 
@@ -50,6 +41,16 @@ function getCurrentDate() {
 function getWeekDay(date) {
     const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
     return days[date.getDay()]
+}
+
+function formatDate(date) {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    console.log(`${month}/${day}/${year}`);
+
+    return `${month}/${day}/${year}`;
 }
 
 
